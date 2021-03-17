dfaca6b5bde0c1abddf9dd18468b5ab156438b5f
noureddein <noureddein@gmail.com>
2021-03-17 20:42:44 +0200

    Done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 'use strict';
+let tuitionValue = [100, 500, 1000, 1200];
 
-function StudentInfo(id, name, studenEmail, mobile, tuition) {
+function StudentInfo(id, name, studenEmail, mobile, tuition, age) {
   this.id = id;
   this.name = name;
   this.studenEmail = studenEmail;
   this.mobile = mobile;
   this.tuition = tuition;
-  this.age = getAge(24, 18);
+  this.age = age;
   StudentInfo.all.push(this);
 }
 StudentInfo.all = [];
@@ -21,14 +22,41 @@ function clickSubmit(event) {
   const mobile = event.target.mobile.value;
   const tuition = event.target.tuition.value;
   const name = studenEmail.substring(0, studenEmail.lastIndexOf('@'));
-  // for (let i = 1; i < localStorage.length; i++) {
-  const student = new StudentInfo(1, name, studenEmail, mobile, tuition);
-  // console.log(student);
-  // break;
-  // }
+  const student = new StudentInfo(id(), name, studenEmail, mobile, tuition, getAge(24, 18));
   localStorage.setItem('student', JSON.stringify(StudentInfo.all));
   student.render();
+  studentForm.reset();
 }
+
+//get student ID
+
+
+const localData = JSON.parse(localStorage.getItem('student'));
+
+let idCounter = 0;
+if (localStorage.length) {
+  idCounter = parseInt(localData[localData.length - 1].id, 10);
+}
+function id() {
+  idCounter++;
+  return idCounter;
+}
+const ids = [];
+const currentYear = new Date().getFullYear();
+// function studentID() {
+//   const randomID = getAge(1, 400);
+//   ids.push(randomID);
+//   if (ids.includes(randomID)) {
+//     randomID;
+//   } else {
+//     console.log(`${currentYear}${randomID}`);
+//   }
+//   console.log('arr', ids);
+//   return `${currentYear}${id()}`;
+// }
+
+// studentID();
+
 
 
 // Get Age....
@@ -36,12 +64,16 @@ function getAge(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const table = document.getElementById('tBody');
 
+//Render Data
+const table = document.getElementById('tBody');
+const totalSpan = document.getElementById('total');
+
+let i = 0;
+let totalTuition = 0;
 StudentInfo.prototype.render = function () {
-  const localData = new StudentInfo(JSON.parse(localStorage.getItem('student')));
-  console.log(localData);
-  for (let i = 0; i < localStorage.length; i++) {
+
+  for (; i < StudentInfo.all.length; i++) {
     //id table data
     const tdElid = document.createElement('td');
     const trEl = document.createElement('tr');
@@ -72,10 +104,52 @@ StudentInfo.prototype.render = function () {
     //tuition table data
     const tdEltTuition = document.createElement('td');
     trEl.appendChild(tdEltTuition);
-    tdEltTuition.textContent = StudentInfo.all[i].tuition;
+    tdEltTuition.textContent = `${StudentInfo.all[i].tuition} JOD`;
 
+    //add delete row button
+    const clearbtn = document.createElement('button');
+    const tdElDelete = document.createElement('td');
+    trEl.appendChild(tdElDelete);
+    tdElDelete.appendChild(clearbtn);
+    clearbtn.textContent = 'Clear';
 
+    // Total tuition
+    totalTuition = parseInt(StudentInfo.all[i].tuition, 10) + totalTuition;
+    totalSpan.textContent = `${totalTuition} JOD`;
 
   }
 
 };
+
+
+function retreive() {
+  if (localStorage.length > 0) {
+    for (let i = 0; i < localData.length; i++) {
+      const x = new StudentInfo(localData[i].id, localData[i].name, localData[i].studenEmail, localData[i].mobile, localData[i].tuition, localData[i].age);
+      x.render();
+    }
+  }
+}
+
+const selectEl = document.getElementById('tuition');
+
+
+
+function tuition() {
+  for (let j in tuitionValue) {
+    const optionEl = document.createElement('option');
+    selectEl.appendChild(optionEl);
+    optionEl.textContent = `${tuitionValue[j]}`;
+  }
+}
+retreive();
+tuition();
+
+function clearTableData() {
+  const tableEL = document.getElementById('tBody');
+  tableEL.remove();
+  localStorage.removeItem('student');
+  totalSpan.textContent = '0 JOD';
+}
+
+
